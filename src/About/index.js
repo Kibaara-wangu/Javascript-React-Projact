eec263d199f914cc555bc9c799b2d5dd18025c31
kibaara-wangu <kibaarawangu@gmail.com>
2024-06-21 19:26:05 +0300

    Styled Users folder

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+import './index.css'; //to link the css file
+const About = () =>{
+    return(
+        <div className="about-text">
+            <p>We are learning about JSX</p>
+            <p>We are coding in a new Front-end Library</p>
+        </div>
+    )
+}
+
+export default About;
